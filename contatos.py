2227ea75e9b347bec469b048fb648e196f5687bb
gustavomotadev <gustavomotadev@gmail.com>
2023-10-15 22:18:08 -0300

    Contact creation

--- contatos.py
+++ contatos.py
@@ -1,19 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 from http import HTTPStatus
-from json import load
+from json import load, dump
 
 def obter_contatos():
     with open('banco.json', 'r', encoding='utf-8') as arquivo_json:
         contatos = load(arquivo_json)
     return contatos
 
+def gravar_contato(novo_contato):
+    with open('banco.json', 'r+', encoding='utf-8') as arquivo_json:
+        contatos = load(arquivo_json)
+        contatos.append(novo_contato)
+        arquivo_json.seek(0)
+        dump(contatos, arquivo_json, indent=True, ensure_ascii=False)
+        arquivo_json.truncate()
+
+    return contatos
+
 app = Flask(__name__)
 
 @app.errorhandler(HTTPStatus.NOT_FOUND)
 def page_not_found(erro):
-   return render_template('404.jinja'), HTTPStatus.NOT_FOUND
+   return (render_template('erro.jinja', codigo=HTTPStatus.NOT_FOUND, 
+        mensagem="Página não encontrada"), HTTPStatus.NOT_FOUND)
 
-@app.route('/contatos')
+@app.errorhandler(HTTPStatus.BAD_REQUEST)
+def page_not_found(erro):
+   return render_template('erro.jinja', codigo=HTTPStatus.BAD_REQUEST,
+        mensagem="Não foi possível realizar operação"), HTTPStatus.BAD_REQUEST
+
+@app.route('/contatos', methods=['GET'])
 def mostrar_contatos():
 
     tipo = request.args.get('tipo')
@@ -22,6 +38,28 @@ def mostrar_contatos():
     
     if tipo:
         contatos = [c for c in contatos if c.get('tipo') == tipo]
+
+    return render_template('contatos.jinja', contatos=contatos)
+
+@app.route('/contatos', methods=['POST'])
+def salvar_contato():
+
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    telefone = request.form.get('telefone')
+    tipo = request.form.get('tipo')
+
+    if not (nome and email and telefone and tipo):
+        abort(HTTPStatus.BAD_REQUEST)
+
+    novo_contato = {
+        "nome": nome,
+        "email": email,
+        "telefone": telefone,
+        "tipo": tipo
+    }
+
+    contatos = gravar_contato(novo_contato)
 
     return render_template('contatos.jinja', contatos=contatos)
 
